4bc719ae9583813944f5753646dd28913ee5078a
Ollie Stevenson <ollie@stevenson.digital>
2020-09-26 16:05:38 +0100

    [add] static desktop styles with gradients

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.scss'
 function App() {
   return (
     <div className="App">
-      <h1 className="f-vogue-90-42">Ollie Stevenson</h1>
-      <h2 className="f-open-38-28">Frontend Engineer @Huel</h2>
-      <p>
-        For public speaking enquiries please email
-        <br/>
-        <a href="mailto:ollie@stevenson.digital">ollie@stevenson.digital</a>
-      </p>
+      <div className="TextWrapper">
+        <h1 className="f-vogue-90-42">Ollie Stevenson</h1>
+        <h2 className="f-open-38-28">Frontend Engineer @Huel</h2>
+        <p>
+          For public speaking enquiries please email
+          <br/>
+          <a href="mailto:ollie@stevenson.digital">ollie@stevenson.digital</a>
+        </p>
+      </div>
+      <div className="Box" />
     </div>
   )
 }
