880c1c22f001b52de2a3a30fef22a5ff03569d8b
Ollie Stevenson <ollie@stevenson.digital>
2020-09-26 15:37:01 +0100

    [change] body font from Brown to Open Sans

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.scss'
 function App() {
   return (
     <div className="App">
-      Ollie Stevenson
+      <h1 className="f-vogue-90-42">Ollie Stevenson</h1>
+      <h2 className="f-open-38-28">Frontend Engineer @Huel</h2>
+      <p>
+        For public speaking enquiries please email
+        <br/>
+        <a href="mailto:ollie@stevenson.digital">ollie@stevenson.digital</a>
+      </p>
     </div>
   )
 }
